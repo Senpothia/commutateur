41625bc97e6449dc2cf64e4913a8798f74ae04ca
Michel <kintelalpz@gmail.com>
2025-08-26 16:21:42 +0200

    correction nom du package

--- src/commutateur/Connecteur.java
+++ src/commutateur/Connecteur.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package keypadprogrammer;
+package commutateur;
 
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
